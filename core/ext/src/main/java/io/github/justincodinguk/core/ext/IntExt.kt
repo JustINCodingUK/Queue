804773540c20d396fbe766ce7a42b037aefcdf91
JustINCodingUK <coderstudent09@gmail.com>
2024-03-05 10:32:05 +0000

    Fix Int.truncatedToString()

--- core/ext/src/main/java/io/github/justincodinguk/core/ext/IntExt.kt
+++ core/ext/src/main/java/io/github/justincodinguk/core/ext/IntExt.kt
@@ -3,10 +3,10 @@ package io.github.justincodinguk.core.ext
 import java.text.DecimalFormat
 
 fun Int.truncatedToString(): String {
-    val df = DecimalFormat("#.##")
+    val df = DecimalFormat("#.#")
     return when {
-        this > 999999 -> "${df.format(this/100.0f)}m"
-        this > 999 -> "${df.format(this/100.0f)}k"
+        this > 999999 -> "${df.format(this/1000000.0f)}m"
+        this > 999  -> "${df.format(this/1000.0f)}k"
         else -> toString()
     }
 }
